32dce2637b3e932c4473767682aee227615f4bb0
Avadhesh kumar singh <63304698+Ava68@users.noreply.github.com>
2023-02-24 14:10:29 +0530

    Create Day-24.java

---
+++ february/Day-24.java
@@ -0,0 +1,31 @@
+/* Intution is in 2 steps
+step 1-> multuply all odd number by 2
+step 2 -> pop max ele from heap diff = Math.min(diff,max-min)
+and add  pQueue.add(max/2); do this untill every even element is exhausted from the queue.
+
+*/
+class Solution {
+    public int minimumDeviation(int[] nums) {
+        PriorityQueue<Integer> pQueue
+            = new PriorityQueue<Integer>(
+                Collections.reverseOrder());
+        int min = Integer.MAX_VALUE;
+        int diff = Integer.MAX_VALUE;
+
+        for(int n:nums)
+        {
+            if(n % 2 != 0)
+                n = n * 2;
+            min = Math.min(min,n);
+            pQueue.add(n);
+        }
+        while(pQueue.peek() %2 == 0)
+        {
+            int max = pQueue.remove();
+            diff = Math.min(diff,max-min);
+            min = Math.min(min,max/2);
+            pQueue.add(max/2);
+        }
+        return Math.min(diff,pQueue.peek() - min);
+    }
+}
